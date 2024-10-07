a747bd7fc6484db9077fec6d4f526a20c5518b0f
alizs10 <ali.text77@gmail.com>
2024-10-07 06:19:59 +0330

    fix mode switcher

--- src/components/game/Difficulties.tsx
+++ src/components/game/Difficulties.tsx
@@ -6,64 +6,25 @@ export default function Difficulties() {
 
     const { difficulty, setDifficulty } = useContext(GameContext);
 
-    const btnsContainerRef = useRef<HTMLDivElement | null>(null);
-    const easyBtnRef = useRef<HTMLButtonElement | null>(null);
-    const mediumBtnRef = useRef<HTMLButtonElement | null>(null);
-    const hardBtnRef = useRef<HTMLButtonElement | null>(null);
-
-    const [left, setLeft] = useState<number>(0);
-    const [width, setWidth] = useState<string>('');
-
 
     function onSwitch(val: TDifficulty) {
         setDifficulty(val);
     }
-
-    useEffect(() => {
-
-        var containerWidth = btnsContainerRef.current?.offsetWidth || 0;
-        var easyBtnWidth = easyBtnRef.current?.offsetWidth || 0;
-        var mediumBtnWidth = mediumBtnRef.current?.offsetWidth || 0;
-        var hardBtnWidth = hardBtnRef.current?.offsetWidth || 0;
-
-        if (difficulty === 0) {
-            setLeft(easyBtnRef.current?.offsetLeft || 0)
-            setWidth((easyBtnWidth / containerWidth) * 100 + '%')
-        }
-
-        if (difficulty === 1) {
-            setLeft(mediumBtnRef.current?.offsetLeft || 0)
-            setWidth((mediumBtnWidth / containerWidth) * 100 + '%')
-        }
-
-        if (difficulty === 2) {
-            setLeft(hardBtnRef.current?.offsetLeft || 0)
-            setWidth((hardBtnWidth / containerWidth) * 100 + '%')
-        }
-
-
-    }, [difficulty])
 
     return (
         <div className={`w-fit mx-auto mt-44 flex flex-col gap-y-4`}>
 
             <label className={`text-3xl text-center text-slate-600 dark:text-slate-300 capitalize`}>select difficulty</label>
 
-            <div ref={btnsContainerRef} className="relative flex p-3 rounded-3xl flex-nowrap gap-x-2 text-slate-700 dark:text-white bg-container">
-                <button ref={easyBtnRef} onClick={() => onSwitch(0)} className={`z-10 px-2 py-1 text-lg capitalize transition-all duration-300 ${difficulty === 0 && 'text-white'}`}>Easy</button>
+            <div className="relative flex p-3 rounded-3xl flex-nowrap text-slate-700 dark:text-white bg-container gap-x-2">
+                <button onClick={() => onSwitch(0)} className={`z-10 ${difficulty === 0 ? 'bg-blue-800 dark:bg-blue-600' : ''} rounded-xl px-2 py-1 text-lg capitalize transition-all duration-300 ${difficulty === 0 && 'text-white'}`}>Easy</button>
 
-                <button ref={mediumBtnRef} onClick={() => onSwitch(1)} className={`z-10 px-2 py-1 text-lg capitalize transition-all duration-300 ${difficulty === 1 && 'text-white'}`}>Medium</button>
+                <button onClick={() => onSwitch(1)} className={`z-10 ${difficulty === 1 ? 'bg-blue-800 dark:bg-blue-600' : ''} rounded-xl px-2 py-1 text-lg capitalize transition-all duration-300 ${difficulty === 1 && 'text-white'}`}>Medium</button>
 
-                <button ref={hardBtnRef} onClick={() => onSwitch(2)} className={`z-10 px-2 py-1 text-lg capitalize transition-all duration-300 ${difficulty === 2 && 'text-white'}`}>Hard</button>
+                <button onClick={() => onSwitch(2)} className={`z-10 ${difficulty === 2 ? 'bg-blue-800 dark:bg-blue-600' : ''} rounded-xl px-2 py-1 text-lg capitalize transition-all duration-300 ${difficulty === 2 && 'text-white'}`}>Hard</button>
 
-                <motion.div
-                    initial={false}
-                    animate={{ width: width ? width : '23%', left: left ? left : 12 }}
-                    className={`absolute z-0 px-3 py-1 text-lg  bg-blue-800 rounded-xl inset-2.5 flex-center dark:bg-blue-600 dark:text-blue-600`}>
-
-                </motion.div>
             </div>
-        </div>
+        </div >
 
     );
 }
--- src/components/game/GameContainer.tsx
+++ src/components/game/GameContainer.tsx
@@ -1,15 +1,9 @@
-import { useContext } from "react";
 import MenuBar from "../menu/MenuBar";
 import StillScreen from "../screens/StillScreen";
 import PlayScreen from "../screens/PlayScreen";
 import WonScreen from "../screens/WonScreen";
-import { GameContext } from "../../context/GameContext";
 
 export default function GameContainer() {
-
-    const { gameState } = useContext(GameContext)
-
-    console.log(gameState)
 
     return (
         <section className="flex flex-col gap-y-4">
