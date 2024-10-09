e28e27e681cbb83864f0e07ac4ebf99f578c2159
alizs10 <ali.text77@gmail.com>
2024-10-10 00:16:28 +0330

    fix

--- src/components/game/CharInputs.tsx
+++ src/components/game/CharInputs.tsx
@@ -9,7 +9,7 @@ export default function CharInputs() {
         // <div className="grid items-center justify-center w-full grid-cols-5 gap-2 rounded-2xl md:grid-cols-7 xl:grid-cols-9">
         <div className="flex flex-wrap items-center justify-center w-full mt-4 gap-x-2 gap-y-4">
 
-            {word.split("").map((char, index) => (
+            {word.split("").map(({ }, index) => (
                 <div key={index} className={`game-char-input ${(playerGuess.length > 0 && playerGuess.find(guess => guess.place === index)?.isCorrect) ? 'game-char-input-correct' : 'game-char-input-default'}`}>
                     {(playerGuess.length > 0 && playerGuess.find(guess => guess.place === index)) ? playerGuess.find(guess => guess.place === index)?.char : '?'}
                 </div>
